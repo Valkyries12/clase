9cc842b1ef269a73d063b9fe127c27c26aafe97e
Nicolas Maximiliano <nico.caruso1992@outlook.com>
2019-09-09 13:28:23 -0300

    Se agrega eejrcicio cat y tail

---
+++ cat.py
@@ -0,0 +1,11 @@
+def cat(archivo):
+    """ imprime el contenido del archivo  """
+    try:
+        with open(archivo) as f:
+            texto = f.readlines()
+            for linea in texto:
+                print(linea)
+    except IOError:
+        print("No se ha encontrado el archivo")
+
+cat("lorem.txt")
---
+++ tail.py
@@ -0,0 +1,12 @@
+def tail(archivo, n:int):
+    """ imprime las n últimas lineas  """
+    try:
+        with open(archivo) as f:
+            texto = f.readlines()
+            for i in range(len(texto)-n, len(texto)):
+                print(texto[i])
+
+    except IOError:
+        print("El archivo no existe")
+
+tail("lorem.txt", 1)
